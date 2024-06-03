bfd31bf6ef3ad8f33583fb38b8c888b1819bc6b8
Pierre Brossard <pierre.brossard496186@gmail.com>
2024-06-03 14:30:02 +0200

    model creation

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,71 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_03_120439) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_03_122818) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "book_genres", force: :cascade do |t|
+    t.bigint "book_id", null: false
+    t.bigint "genre_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["book_id"], name: "index_book_genres_on_book_id"
+    t.index ["genre_id"], name: "index_book_genres_on_genre_id"
+  end
+
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.integer "serie_number"
+    t.string "type"
+    t.string "cover_url"
+    t.text "description"
+    t.string "isbn"
+    t.date "release"
+    t.string "author"
+    t.string "illustrator"
+    t.string "edition"
+    t.string "illustrations"
+    t.bigint "serie_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["serie_id"], name: "index_books_on_serie_id"
+  end
+
+  create_table "collections", force: :cascade do |t|
+    t.boolean "is_read"
+    t.boolean "is_favorited"
+    t.text "comment"
+    t.bigint "book_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["book_id"], name: "index_collections_on_book_id"
+    t.index ["user_id"], name: "index_collections_on_user_id"
+  end
+
+  create_table "favorite_series", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "serie_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["serie_id"], name: "index_favorite_series_on_serie_id"
+    t.index ["user_id"], name: "index_favorite_series_on_user_id"
+  end
+
+  create_table "genres", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "series", force: :cascade do |t|
+    t.string "name"
+    t.integer "books_total"
+    t.string "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -26,4 +88,11 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_03_120439) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "book_genres", "books"
+  add_foreign_key "book_genres", "genres"
+  add_foreign_key "books", "series", column: "serie_id"
+  add_foreign_key "collections", "books"
+  add_foreign_key "collections", "users"
+  add_foreign_key "favorite_series", "series", column: "serie_id"
+  add_foreign_key "favorite_series", "users"
 end
